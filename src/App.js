324ce828c2f399593e8c5abbd609e75284fb4e3a
Lokesh S <153573033+lokesh-weby@users.noreply.github.com>
2024-06-08 09:03:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
   gsap.from(".info",{
     scrollTrigger:{
       trigger:".info",
-      start:"top 50px",
+      start:"top 20px",
       scrub:true,
       pin:true,
       markers:true
