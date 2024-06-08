7b421947878bcd50c51b4535d2c5b78b38a2aae9
Lokesh S <153573033+lokesh-weby@users.noreply.github.com>
2024-06-08 11:06:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
       scrollTrigger: {
         trigger: "#target",
         start: "top 40%",    
-        scrub:3,
+        scrub:1,
       },  
        opacity:0,
       scale:2,
