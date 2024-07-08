8d3ae6b358a55d22054e32aa609864988deb2ece
Lokesh S <153573033+lokesh-weby@users.noreply.github.com>
2024-07-08 18:19:14 +0530

    Update footer.js

--- src/footer.js
+++ src/footer.js
@@ -17,6 +17,9 @@ const footer = () => {
         <h2><a href="#projects">Projects</a></h2>
         <h2><a href="#education">Education</a></h2>
         </div>
+    <!-- Start of CuterCounter Code -->
+<a href="https://www.cutercounter.com/" target="_blank"><img src="https://www.cutercounter.com/hits.php?id=hxppokq&nd=6&style=3" border="0" alt="best free website hit counter"></a>
+<!-- End of CuterCounter Code -->
 
     </footer>
       
